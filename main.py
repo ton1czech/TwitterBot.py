5f259325c919313544527256a53d360e71508861
ton1czech <tonyasek007@gmail.com>
2021-04-17 11:49:29 +0200

    cleaned up the code

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+# Import Modules
 import tweepy
 import pandas_datareader.data as data
 import datetime as dt
 
+# Set up Twitter API
 with open('keys.txt') as f:
     content = f.read().splitlines()
 
@@ -12,11 +14,12 @@ access_token_secret = content[3]
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
-
-price = data.get_quote_yahoo(['BTC-USD', 'ETH-USD', 'DOGE-USD', 'CZK=X', 'EURCZK=X'])['price']
-print(price)
-
 api = tweepy.API(auth)
+
+# Get real-time prices
+price = data.get_quote_yahoo(['BTC-USD', 'ETH-USD', 'DOGE-USD', 'CZK=X', 'EURCZK=X'])['price']
+
+# The actual tweet
 api.update_status(status = f"""
 Bitcoin: {price[0]} $
 Ethereum: {price[1]} $
