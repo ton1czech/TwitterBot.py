d8c623e7a1b3f4e216e914402afce9b1b2a77484
ton1czech <tonyasek007@gmail.com>
2021-06-05 21:46:49 +0200

    🎁NEW: fetching weather

--- src/weather/main.py
+++ src/weather/main.py
@@ -4,8 +4,6 @@ from os import environ
 from dotenv import load_dotenv
 from datetime import datetime
 
-from pprint import pprint
-
 load_dotenv()
 
 lat = 49.395555
@@ -19,10 +17,8 @@ def get_weather():
     url = f"https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={drop}&units={units}&lang={lang}&appid={environ['API_key']}"
     weather = requests.get(url).json()
 
-    date = weather['hourly'][0]['dt']
-    date = datetime.fromtimestamp(date).strftime("%H")
-
+    date = datetime.fromtimestamp(weather['hourly'][0]['dt']).strftime("%H")
     temp = weather['hourly'][0]['temp']
-    print(temp)
+    state = weather['hourly'][0]['weather'][0]['description']
 
 get_weather()
