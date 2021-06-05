97e2b87f3fde34f2e672d8a0c396b358fbc762e6
ton1czech <tonyasek007@gmail.com>
2021-06-05 21:29:44 +0200

    🎁NEW: weather fetcher (openWeather)

---
+++ src/weather/main.py
@@ -0,0 +1,25 @@
+# Import modules
+import requests
+from os import environ
+from dotenv import load_dotenv
+from datetime import datetime
+
+from pprint import pprint
+
+load_dotenv()
+
+lat = 49.395555
+lon = 13.295094
+drop = 'current,minutely,daily,alerts'
+units = 'metric'
+
+# Get weather forecast in Klatovy for today
+def get_weather():
+    url = f"https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={drop}&units={units}&appid={environ['API_key']}"
+    weather = requests.get(url).json()
+
+    date = weather['hourly'][0]['dt']
+    date = datetime.fromtimestamp(date).strftime("%H")
+    print(date)
+
+get_weather()
