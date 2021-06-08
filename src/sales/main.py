fda8bc34ac0407e61c351b2d284ad9a6ecc08ed6
ton1czech <tonyasek007@gmail.com>
2021-06-08 11:51:34 +0200

    🎁NEW: sales module

---
+++ src/sales/main.py
@@ -0,0 +1,29 @@
+import requests
+import re
+from bs4 import BeautifulSoup
+
+def fetch_sales():
+    url = 'https://gg.deals'
+
+    soup = BeautifulSoup(requests.get(url).text, 'lxml')
+    
+    column = soup.find_all('div', class_="list")[1]
+
+    titles, prices, stores = [], [], []
+
+    for id in range(0,10):
+        game = column.find_all('div', class_="mainpage-preset-item")[id]
+
+        title = game.find('a', class_="ellipsis title").text
+        titles.append(title)
+
+        price = game.find('span', class_="numeric").text
+        prices.append(price)
+
+        store = game.find('div', class_="tag-shop ellipsis tag with-bull")
+        store = store.find('span', class_="value").text
+        stores.append(store)
+
+    print(titles, prices, stores)
+
+fetch_sales()
