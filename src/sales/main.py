5094cac389859280131266bb537f85b1ded5b755
ton1czech <tonyasek007@gmail.com>
2021-06-09 16:06:06 +0200

    👌IMPROVE: code refactor

--- src/sales/main.py
+++ src/sales/main.py
@@ -1,34 +1,22 @@
 import requests
-import re
 from bs4 import BeautifulSoup
 
 def fetch_sales():
-    url = 'https://gg.deals'
+    games = [[],[],[]]
 
-    soup = BeautifulSoup(requests.get(url).text, 'lxml')
-    
+    soup = BeautifulSoup(requests.get('https://gg.deals').text, 'lxml')
     column = soup.find_all('div', class_="list")[1]
-
-    titles, prices, stores = [], [], []
 
     for id in range(0,10):
         game = column.find_all('div', class_="mainpage-preset-item")[id]
 
         title = game.find('a', class_="ellipsis title").text
-        titles.append(title)
+        games[0].append(title)
 
         price = game.find('span', class_="numeric").text
-        prices.append(price)
+        games[1].append(price)
 
-        store = game.find('div', class_="tag-shop ellipsis tag with-bull")
-        store = store.find('span', class_="value").text
-        stores.append(store)
+        store = game.find('div', class_="tag-shop ellipsis tag with-bull").find('span', class_="value").text
+        games[2].append(store)
     
-        if prices[id] != "Free":
-            prices[id] = None
-        
-        print(prices)
-
-    return titles, prices, stores
-
-fetch_sales()
+    return games
