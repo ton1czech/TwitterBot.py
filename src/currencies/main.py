10f06334f4ae5c9a819c3749223a05f5f0caf4ab
ton1czech <tonyasek007@gmail.com>
2021-06-13 13:00:44 +0200

    ❌REMOVED: useless comment

--- src/currencies/main.py
+++ src/currencies/main.py
@@ -2,6 +2,6 @@ import pandas_datareader.data as data
 
 def fetch_currencies():
     currencies = data.get_quote_yahoo(['BTC-USD', 'ETH-USD', 'DOGE-USD', 'CZK=X', 'EURCZK=X'])['price']
-    prices = [format(currency, ",.3f") for currency in currencies]      # store all currencies inside list
+    prices = [format(currency, ",.3f") for currency in currencies]
 
     return prices
