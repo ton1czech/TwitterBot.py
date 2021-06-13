b0c17a7f7673b2d3429b9c5bef632b223e4f419c
ton1czech <tonyasek007@gmail.com>
2021-06-13 13:12:30 +0200

    👌IMPROVE: squashing the code

--- src/sales/main.py
+++ src/sales/main.py
@@ -10,13 +10,8 @@ def fetch_sales():
     for id in range(0,10):
         game = column.find_all('div', class_="mainpage-preset-item")[id]
 
-        title = game.find('a', class_="ellipsis title").text
-        games[0].append(title)
-
-        price = game.find('span', class_="numeric").text
-        games[1].append(price)
-
-        store = game.find('div', class_="tag-shop ellipsis tag with-bull").find('span', class_="value").text
-        games[2].append(store)
+        games[0].append(game.find('a', class_="ellipsis title").text)   # title
+        games[1].append(game.find('span', class_="numeric").text)       # price
+        games[2].append(game.find('div', class_="tag-shop ellipsis tag with-bull").find('span', class_="value").text) # store
     
     return games
