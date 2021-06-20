0b9efa3cb4ca1a7b1ec6d9cef273d5dcbec3ff05
ton1czech <tonyasek007@gmail.com>
2021-06-20 12:40:47 +0200

    👌IMPROVE: Filtering non free games

--- src/game_sales/main.py
+++ src/game_sales/main.py
@@ -10,8 +10,13 @@ def fetch_game_sales():
     for id in range(0,10):
         game = column.find_all('div', class_="mainpage-preset-item")[id]
 
-        games[0].append(game.find('a', class_="ellipsis title").text)   # title
-        games[1].append(game.find('span', class_="numeric").text)       # price
-        games[2].append(game.find('div', class_="tag-shop ellipsis tag with-bull").find('span', class_="value").text) # store
-    
+        title = game.find('a', class_="ellipsis title").text
+        price = game.find('span', class_="numeric").text
+        store = game.find('div', class_="tag-shop ellipsis tag with-bull").find('span', class_="value").text
+
+        if price == "Free":
+            games[0].append(title)
+            games[1].append(price)
+            games[2].append(store)
+
     return games
